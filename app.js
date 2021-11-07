9cafed7c190123985afb484bc7881f19ada0856a
dedekm <dedekmm@gmail.com>
2021-11-07 19:51:11 +0100

    add json error handler

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const debug = require('debug')('fikmat-api:server');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const port = process.env.PORT || '8020'
+const port = process.env.PORT || '8020';
 
 app.use(express.json());
+
+const jsonErrorHandler = (err, req, res, next) => {
+  res.status(500).send({ error: err });
+}
+app.use(jsonErrorHandler);
 
 const rateLimit = require("express-rate-limit")
 const limiter = rateLimit({
